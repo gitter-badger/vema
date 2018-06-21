ca31061fd6441e40faac61d702e7fe023e4b266b
Mohamed Altarawy <35356629+Altarawy@users.noreply.github.com>
2018-06-21 19:33:38 -0400

    Delete statuslinecolumn.py

--- src/statuslinecolumn.py
+++
@@ -1,5 +0,0 @@
-def StatusLineColumn(self, e):
-    line = self.control.GetCurrentLine() + 1
-    col = self.control.GetColumn(self.control.GetCurrentPos())
-    stat = "Line %s, Column %s" % (line, col)
-    self.statusbar.SetStatusText(stat, 1)
